be801d7a026f968eb37309c2986697cecdf70b09
alik-balika <balikaalik@gmail.com>
2024-02-13 17:34:14 -0800

    display list of names and open modal when user presses a name in the list (mobile version)

--- src/App.jsx
+++ src/App.jsx
@@ -12,27 +12,3 @@ const App = () => {
 };
 
 export default App;
-
-// import React from "react";
-// import { Box } from "@mui/material";
-
-// const App = () => {
-//   return (
-//     <Box
-//       sx={{
-//         height: "300px",
-//         width: {
-//           xs: 100,
-//           sm: 200,
-//           md: 300,
-//           lg: 400,
-//           xl: 500,
-//         },
-//         bgcolor: "primary.main",
-//         margin: "20px",
-//       }}
-//     ></Box>
-//   );
-// };
-
-// export default App;
--- src/components/PageContent.jsx
+++ src/components/PageContent.jsx
@@ -1,7 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
+import { modalStyle, people } from "../data.js";
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Modal,
+  Typography,
+} from "@mui/material";
 
 const PageContent = () => {
-  return <div>PageContent</div>;
+  const [open, setOpen] = useState(false);
+  const [selectedPerson, setSelectedPerson] = useState(null);
+
+  const handleOpen = (person) => {
+    setSelectedPerson(person);
+    setOpen(true);
+  };
+  const handleClose = () => setOpen(false);
+
+  return (
+    <>
+      <List
+        sx={{
+          width: {
+            md: "300px",
+          },
+        }}
+      >
+        {people.map((person, index) => (
+          <ListItem
+            key={index}
+            divider
+            sx={{ borderRight: "1px solid rgba(0, 0, 0, 0.12)" }}
+          >
+            <ListItemButton
+              sx={{
+                textAlign: {
+                  xs: "center",
+                  md: "left",
+                },
+              }}
+              onClick={() => handleOpen(person)}
+            >
+              <ListItemText primary={person.name} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Modal open={open} onClose={handleClose}>
+        {selectedPerson ? (
+          <Box sx={modalStyle}>
+            <Typography variant="h4">{selectedPerson.name}</Typography>
+            <Typography variant="h5">{selectedPerson.birthday}</Typography>
+            <Typography variant="h6">{selectedPerson.phoneNumber}</Typography>
+            <Typography variant="h6" sx={{ marginTop: "10px" }}>
+              {selectedPerson.address}
+            </Typography>
+          </Box>
+        ) : (
+          <></>
+        )}
+      </Modal>
+    </>
+  );
 };
 
 export default PageContent;
